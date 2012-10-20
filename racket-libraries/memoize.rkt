2299527768c324e83f85991bcf102a35d1c99dc0
JP Verkamp <verkampj@indiana.edu>
2012-10-20 11:50:15 -0400

    Updated define-memoized with cleaner quasiquoted version on suggestion from programmingpraxis@gmail.com

--- racket-libraries/memoize.rkt
+++ racket-libraries/memoize.rkt
@@ -3,7 +3,7 @@
 (provide define-memoized)
 
 ; replace define with a memoized version
-(define-syntax define-memoized
+(define-syntax define-memoized-old
   (syntax-rules ()
     [(_ (f args ...) bodies ...)
      (define f
@@ -19,6 +19,20 @@
               (hash-ref results vals))
             args ...))))]))
 
+; replace define with a memoized version
+(define-syntax define-memoized
+  (syntax-rules ()
+    [(_ (f args ...) bodies ...)
+     (define f
+       ; store the cache as a hash of args => result       
+       (let ([cache (make-hash)])
+         (lambda (args ...)
+           ; if we haven't calculated it before, do so now
+           (when (not (hash-has-key? cache `(,args ...)))
+             (hash-set! cache `(,args ...) (begin bodies ...)))
+           ; return the cached result
+           (hash-ref cache `(,args ...)))))]))
+       
 ; example, fibonacci without memoization
 (define (fib n)
   (cond
@@ -26,7 +40,19 @@
     [else (+ (fib (- n 1)) (fib (- n 2)))]))
 
 ; example, fibonacci with memoization
-(define-memoized (mfib n)
+(define-memoized-old (mfib n)
   (cond
     [(< n 1) 1]
     [else (+ (mfib (- n 1)) (mfib (- n 2)))]))
+
+; example, fibonacci with memoization using the second macro
+(define-memoized (mfib2 n)
+  (cond
+    [(< n 1) 1]
+    [else (+ (mfib2 (- n 1)) (mfib2 (- n 2)))]))
+
+(define n 100000)
+(collect-garbage)
+(time (let ([result (mfib n)]) n))
+(collect-garbage)
+(time (let ([result (mfib2 n)]) n))
