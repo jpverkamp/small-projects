fe55f37b3a2f1f75e72a60aa2fe15e3203f5d7e2
JP Verkamp <verkampj@indiana.edu>
2013-08-06 12:33:25 -0400

    Swapped with-handlers for dynamic-wind; added thread on read.

--- blog/with-gzip.rkt
+++ blog/with-gzip.rkt
@@ -5,34 +5,42 @@
 
 (provide/contract 
  (with-gzip
-  (-> (-> any) #:buffer-size [or/c false? exact-positive-integer?] any)))
+   (->* ((-> any))
+        (#:buffer-size (or/c false? exact-positive-integer?))
+        any)))
 
 (define (with-gzip thunk #:buffer-size [buffer-size #f])
   (define-values (pipe-from pipe-to) (make-pipe buffer-size))
-  (with-handlers ([exn:fail?
-                   (λ (err)
-                     (close-output-port pipe-to)
-                     (close-input-port pipe-from)
-                     (error 'with-gzip (exn-message err)))])
-    (gzip-through-ports (current-input-port) pipe-to #f (current-seconds))
-    (close-output-port pipe-to)
-    (parameterize ([current-input-port pipe-from])
-      (thunk))
-    (close-input-port pipe-from)))
+  (dynamic-wind
+   void
+   (λ ()
+     (thread
+      (λ ()
+        (gzip-through-ports (current-input-port) pipe-to #f (current-seconds))
+        (close-output-port pipe-to)))
+     (parameterize ([current-input-port pipe-from])
+       (thunk)))
+   (λ ()
+     (unless (port-closed? pipe-to) (close-output-port pipe-to))
+     (unless (port-closed? pipe-from) (close-input-port pipe-from)))))
 
 (provide/contract 
  (with-gunzip 
-  (-> (-> any) #:buffer-size [or/c false? exact-positive-integer?] any)))
+   (->* ((-> any))
+        (#:buffer-size (or/c false? exact-positive-integer?))
+        any)))
                   
 (define (with-gunzip thunk #:buffer-size [buffer-size #f])
   (define-values (pipe-from pipe-to) (make-pipe buffer-size))
-  (with-handlers ([exn:fail?
-                   (λ (err)
-                     (close-output-port pipe-to)
-                     (close-input-port pipe-from)
-                     (error 'with-gunzip (exn-message err)))])
-    (gunzip-through-ports (current-input-port) pipe-to)
-    (close-output-port pipe-to)
-    (parameterize ([current-input-port pipe-from])
-      (thunk))
-    (close-input-port pipe-from)))
+  (dynamic-wind
+   void
+   (λ ()
+     (thread 
+      (λ ()
+        (gunzip-through-ports (current-input-port) pipe-to)
+        (close-output-port pipe-to)))
+     (parameterize ([current-input-port pipe-from])
+       (thunk)))
+   (λ ()
+     (unless (port-closed? pipe-to) (close-output-port pipe-to))
+     (unless (port-closed? pipe-from) (close-input-port pipe-from)))))
