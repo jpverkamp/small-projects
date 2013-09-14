b7b0528e48fdfdbb1791fbeb3fa163ebea3ed278
JP Verkamp <github@jverkamp.com>
2013-09-13 23:34:29 -0400

    Added comments

--- blog/dh/dh-shared.rkt
+++ blog/dh/dh-shared.rkt
@@ -4,17 +4,21 @@
 
 (provide (all-defined-out))
 
+; Maximum value for randomly generated primes
 (define RANGE (expt 2 16))
 
+; Set to print debugging messages on send/recv
 (define dh-debug-mode (make-parameter #f))
 (define dh-debug-id   (make-parameter (random RANGE)))
 
+; Receive a message from a port, must be Racket readable
 (define (recv from)
   (define value (read from))
   (when (dh-debug-mode)
     (printf "~a recv (~a): ~a\n" (dh-debug-id) from value))
   value)
 
+; Send a value to a port; write a newline and flush to force send
 (define (send value to)
   (when (dh-debug-mode)
     (printf "~a send (~a): ~a\n" (dh-debug-id) to value))
@@ -22,6 +26,7 @@
   (newline to)
   (flush-output to))
 
+; Use XOR and Base64 to encode a message, depends on random seed
 (define (xor-encode message)
   (base64-encode
    (string->bytes/utf-8
@@ -30,6 +35,7 @@
        (integer->char (bitwise-xor (random 256) (char->integer c))))))
    #""))
 
+; Decode a message encoded with the previous, depends on random seed
 (define (xor-decode response)
   (list->string
    (for/list ([c (in-string (bytes->string/utf-8 (base64-decode response)))])
