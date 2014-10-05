c9f2344dcbe634d550f5c78f6092934944bb78e0
JP Verkamp <me@jverkamp.com>
2014-10-05 15:39:54 -0700

    Update regex-fractal.rkt

--- blog/regex-fractal.rkt
+++ blog/regex-fractal.rkt
@@ -6,26 +6,26 @@
 (define (size->path-length size)
   (inexact->exact (floor (/ (log size) (log 2)))))
 
+(define current-size     (make-parameter 64))
+(define current-coloring (make-parameter (thunk* '#(1 1 1))))
+(define current-mode     (make-parameter 'short))
+
 ; Generate a fractal by matching a recursive path into an image
-; #:coloring is a function that will be applied to the match result and should return a color
-; #:short-circuit means that the function stops as soon as the regex matches (default #t)
-(define (regex-fractal 
-         regex size 
-         #:coloring [coloring (thunk* '#(1 1 1))] 
-         #:short-circuit [short-circuit #t])
+(define (regex-fractal regex)
   (flomap->bitmap
    (build-flomap*
-    3 size size
+    3 (current-size) (current-size)
     (λ (x y)
-      (let loop ([t 0] [l 0] [s size] [path ""])
+      (let loop ([t 0] [l 0] [s (current-size)] [path ""])
         (cond
           ; If we're short circuiting, check the regex; if it matches, apply the coloring
-          [(and short-circuit (regexp-match regex path)) => coloring]
+          [(and (eq? (current-mode) 'short)
+                (regexp-match regex path)) => (current-coloring)]
           ; If we're at the last level, always apply the coloring function
           ; If it fails, always return black 
           [(<= s 1)
            (cond
-             [(regexp-match regex path) => coloring]
+             [(regexp-match regex path) => (current-coloring)]
              [else '#(0 0 0)])]
           ; Otherwise, divide the region into four subregions and recur into the proper one
           [else
@@ -38,95 +38,106 @@
             s/2
             (~a path 
                 (match (list (< y y-mid) (< x x-mid))
-                  ['(#t #t) 1]
-                  ['(#t #f) 2]
+                  ['(#t #t) 2]
+                  ['(#t #f) 1]
                   ['(#f #t) 3]
                   ['(#f #f) 4])))]))))))
 
 ; ----- Examples -----
 
-(define (demo size)
-  (for/list ([f (in-list (list sierpinski 
-                               sierpinski/length 
-                               four-colors 
-                               left/right 
-                               jagged 
-                               double-sierpinski 
-                               ones-then-twos
-                               ascending
-                               no-one
-                               even-sum
-                               odd-sum
-                               another-sierpinski
-                               ones-at-the-end))])
-    (list f (f size))))
-
-; Calculate a sierpinski triangle by lighlighting anything with a 1
-(define (sierpinski size) (regex-fractal #px"1" size))
-
-; Do the same, but highlight each point by how far it took to get the match
-(define (sierpinski/length size)
-  (define path-length (size->path-length size))
-  (regex-fractal
-   #px".*1.*" size
-   #:coloring
+(define all-colorings
+  (hash
+   ; Basic coloring, all matching pixels are white
+   'default      
+   (thunk* '#(1 1 1))
+   ; Make a gradient based on the length of the capturing groups
+   ; If the group is full length, use white
+   ; 1 group = green only
+   ; 2 groups = green then red
+   ; 3 groups = RGB
+   'by-length
    (λ (m)
      (define l (string-length (car m)))
-     (if (= l path-length)
+     (define p (size->path-length (current-size)))
+     (if (= l p)
          '#(1 1 1)
-         (vector 0 (/ l path-length) 0)))))
-
-; Calculate the longest sequence and map to different colors
-; Disable short circuiting so that we get a full length match 
-(define (four-colors size)
-  (define path-length (size->path-length size))
-  (regex-fractal
-   #px"((.)(\\2*))" size
-   #:short-circuit #f
-   #:coloring
+         (vector
+          (if (>= (length m) 3) (/ (string-length (list-ref m 2)) p) 0)
+          (if (>= (length m) 2) (/ (string-length (list-ref m 1)) p) 0)
+          (if (>= (length m) 4) (/ (string-length (list-ref m 3)) p) 0))))
+   ; Color based on the most common number
+   'common-voting
    (λ (m)
-     (match m
-       [#f '#(0 0 0)]
-       [(list all _ which _)
-        (define g (/ (string-length all) path-length))
-        (case which
-          [("1") (vector g g g)]
-          [("2") (vector g 0 0)]
-          [("3") (vector 0 g 0)]
-          [("4") (vector 0 0 g)])]))))
+     (define-values (which _)
+       (for/fold ([max-i #f] [max-v 0])
+                 ([i (in-range 1 5)])
+         (define v (count 
+                    (λ (c) (= i (string->number (string c))))
+                    (string->list (car m))))
+         (if (> v max-v)
+             (values i v)
+             (values max-i max-v))))
+     
+     (define l (string-length (car m)))
+     (define p (size->path-length (current-size)))
+     (define g (/ l p))
+                          
+     (case which
+       [(1)  (vector g g g)]
+       [(2)  (vector g 0 0)]
+       [(3)  (vector 0 g 0)]
+       [(4)  (vector 0 0 g)]
+       [else (vector 0 0 0)]))
+   ))
 
-; Split to either left or right pixels, matches either the first 1 or 2
-(define (left/right size)
-  (regex-fractal 
-   #px"(1|2)" size
-   #:coloring
-   (λ (m)
-     (match m
-       [(list _ "1") '#(1 1 1)]
-       [(list _ "2") '#(0 0 0)]))))
+(define all-regexes
+  (hash
+   'sierpinski        #px".*1.*"
+   'four-corners      #px"((.)(\\2*))"
+   'left-right        #px"(1|2)"
+   'jagged            #px"(12)"
+   'double-sierpinski #px"(1.*2|2.*1)"
+   'ones-then-twos    #px"^[34]*[134]*[34]*[234]*[34]*$"
+   'ascending         #px"^1*2*3*4*$"
+   'no-one            #px"^[^1]*$"
+   'even-sum          #px"^(2|4|[13][24]*[13])*$"
+   'odd-sum           #px"^(2|4|[13][24]*[13])*[13](2|4|[13][24]*[13])*$"
+   'ones-at-the-end   #px"1$"
+   ; comments @ https://www.reddit.com/r/dailyprogrammer/comments/2fkh8u/
+   'curls             #px"[13][24][^1][^2][^3][^4]"
+   'self-similar      #px"(.)\\1..\\1"
+   'thumbsup          #px".*1*2[^24]34*(.*)"
+   'boxes             #px"(?:13|31)(.*)"
+   'spiky             #px"(12|23|34)"
+   'sierpinski-nest   #px"4[^4][^4]2(.*)"
+   ; https://imgur.com/a/QWMGi
+   'boxes             #px"(13|31|24|42)"
+   'boxes-remainder   #px"(?:13|31|24|42)(.*)"
+   'squared-carpet    #px"(13|31)"
+   'outlined          #px"(1[124]|2[14]|5[12|31])*"
+   'figure-eights     #px"(?:..)*(?:[13][13]|[24][24])((?:..)*)"
+   'scanlines         #px"^[13]*[24]*$"
+   ))
 
-; Match a 1 followed immediately by a 2 to get a neat jagged shape
-(define (jagged size) (regex-fractal #px"(12)" size))
+(define (demo folder sizes)
+  (make-directory* folder) ; NOP if folder exists
+  (for* ([(regex-name    regex)    (in-hash all-regexes)]
+         [mode                     (in-list '(short full))]
+         [(coloring-name coloring) (in-hash all-colorings)]
+         [size                     (in-list (if (list? sizes) sizes (list sizes)))])
+    (parameterize ([current-size size]
+                   [current-coloring coloring]
+                   [current-mode mode])
+      
+      (define path 
+        (build-path folder 
+                    (~a regex-name "_" mode "_" coloring-name "_" size ".png")))
 
-; Match two sierpinski trianges (1 and 2)
-(define (double-sierpinski size) (regex-fractal #px"(1.*2|2.*1)" size))
-
-; Match patterns that have all 1s before all 2s (3s and 4s anywhere) 
-; Interestingly, this ends up making a very similar shape to the double-sierpinski
-(define (ones-then-twos size) (regex-fractal "^[34]*[134]*[34]*[234]*[34]*$" size #:short-circuit #f))
-
-; Only images with strictly ascending points all the way down
-(define (ascending size) (regex-fractal "^1*2*3*4*$" size #:short-circuit #f))
-
-; Images without ones, of course this is the inverse of a sierpinski triangle
-(define (no-one size) (regex-fractal "^[^1]*$" size #:short-circuit #f))
-
-; Match regular expressions where the digits sum to an even/odd number
-(define (even-sum size) (regex-fractal "^(2|4|[13][24]*[13])*$" size #:short-circuit #f))
-(define (odd-sum size)  (regex-fractal "^(2|4|[13][24]*[13])*[13](2|4|[13][24]*[13])*$" size #:short-circuit #f))
-
-; Another way to get the sierpinski triangle, anything that ends with ones
-(define (another-sierpinski size) (regex-fractal "1$" size))
-
-; Dots: anything that ends with ones, but this time don't circuit
-(define (ones-at-the-end size) (regex-fractal "1$" size #:short-circuit #f))
+      (cond
+        [(file-exists? path)
+         (printf "~a (exists)\n" path)]
+        [else
+         (displayln path)
+         (flush-output)
+         (define img (regex-fractal regex))
+         (send img save-file path 'png)]))))
